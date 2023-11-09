7e759125bdab13c908d6dd1a99ae9e3c789a3d95
David Helguero <dahelguero@gmail.com>
2023-11-09 13:20:05 -0300

    first commit

---
+++ src/main/java/sugeridor/Sugeridor.java
@@ -0,0 +1,51 @@
+package sugeridor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+@SuppressWarnings("deprecation")
+public class Sugeridor implements Observer{
+	
+		private final OkHttpClient client = new OkHttpClient();
+	    private final ObjectMapper mapper = new ObjectMapper();
+
+	    public List<String> buscarSugerencias(List<String> productos){
+	        String url = "https://recomendador-de-productos-production.up.railway.app/api/v1/recomendador/productosSugeridos";
+	        String json;
+			try {
+				json = mapper.writeValueAsString(productos);
+			} catch (JsonProcessingException e1) {
+				return null;
+			}
+	        RequestBody body = RequestBody.create(
+	                MediaType.parse("application/json; charset=utf-8"), json);
+	        Request request = new Request.Builder()
+	                .url(url)
+	                .post(body)
+	                .build();
+	        try (Response response = client.newCall(request).execute()) {
+	            return Arrays.asList(mapper.readValue(response.body().string(), String[].class));
+	        }
+	        catch(Exception e) {
+	        	return null;
+	        }
+	    
+    }
+
+		@Override
+		public void update(Observable o, Object arg) {
+			//if(o instanceof Core)
+			
+		}
+}
